fb43c4f8627d1a1fc78706c717ecca67d33624c3
grazomarin <huseynovk355@gmail.com>
2022-08-16 11:13:52 +0400

    the game now logs a winner

--- script.js
+++ script.js
@@ -1,22 +1,43 @@
-const cells = document.querySelectorAll('[data-cell]')
+const X_CLASS = 'x'
+const O_CLASS = 'o'
+const WINNING_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5], 
+    [6, 7 ,8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
 const board = document.getElementById('board')
+const cellElements = document.querySelectorAll('[data-cell]')
 
-cells.forEach(cell => {
+
+cellElements.forEach(cell => {
     cell.addEventListener('click', () => {
-        if (cell.classList.length < 2){
-            cell.classList.add(board.classList[1])
-            changePlayer();
+        cell.classList.add(board.classList[1])
+        if (checkWin(board.classList[1])) {
+            console.log('winner')
         }
-        return
-    })
+        changePlayer();
+    }, { once: true })
 });
 
 function changePlayer () {
-    if (board.classList[1] === 'x') {
-        board.classList = 'board o'
-        console.log('o')
+    if (board.classList[1] === X_CLASS) {
+        board.classList.add(O_CLASS)
+        board.classList.remove(X_CLASS)
     } else {
-        board.classList = 'board x'
-        console.log('x')
+        board.classList.add(X_CLASS)
+        board.classList.remove(O_CLASS)
     }
+}
+
+function checkWin(currentClass) {
+    return WINNING_COMBINATIONS.some(combination => {
+        return combination.every(index => {
+            return cellElements[index].classList.contains(currentClass)
+        })
+    })
 }
